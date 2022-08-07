1ccedd444835bb4bcfa49481a9e92f1e4af17799
EvilTopHat <110784648+EvilTopHat@users.noreply.github.com>
2022-08-07 14:39:00 -0400

    added first version of script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,18 @@
+echo "script must be run as root"
+#setup firewall to block all but ssh
+ufw allow ssh
+#update software
+apt-get update && apt-get upgrade -y
+#setup automatic updates TODO test
+#apt-get install unattended-upgrades
+#systemctl enable unattended-upgrades #should already be enabled but this line is just for double chekcing
 
+#install apache
+
+#install mysql
+
+#install wordpress
+
+#install diode and publish new site
+#curl -Ssf https://diode.io/install.sh | sh
+#diode publish -public 80:80 
