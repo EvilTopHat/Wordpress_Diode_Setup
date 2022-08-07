a8c97aa54f1f5435293070cc9cceef989593f063
EvilTopHat <110784648+EvilTopHat@users.noreply.github.com>
2022-08-07 14:49:47 -0400

    added some more instructions for installing wordpress

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 echo "script must be run as root"
+echo "if prompted press accept the qustions in the prompts to continue"
 #setup firewall to block all but ssh
 ufw allow ssh
+ufw enable
 #update software
 apt-get update && apt-get upgrade -y
 #setup automatic updates TODO test
@@ -8,11 +10,44 @@ apt-get update && apt-get upgrade -y
 #systemctl enable unattended-upgrades #should already be enabled but this line is just for double chekcing
 
 #install apache
-
-#install mysql
-
+#https://ubuntu.com/tutorials/install-and-configure-wordpress#2-install-dependencies
+apt-get install apache2 \
+                 ghostscript \
+                 libapache2-mod-php \
+                 mysql-server \
+                 php \
+                 php-bcmath \
+                 php-curl \
+                 php-imagick \
+                 php-intl \
+                 php-json \
+                 php-mbstring \
+                 php-mysql \
+                 php-xml \
+                 php-zip
 #install wordpress
-
+mkdir -p /srv/www
+chown www-data: /srv/www
+curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+#Configure Apache for WordPress
+echo "<VirtualHost *:80>
+    DocumentRoot /srv/www/wordpress
+    <Directory /srv/www/wordpress>
+        Options FollowSymLinks
+        AllowOverride Limit Options FileInfo
+        DirectoryIndex index.php
+        Require all granted
+    </Directory>
+    <Directory /srv/www/wordpress/wp-content>
+        Options FollowSymLinks
+        Require all granted
+    </Directory>
+</VirtualHost>" > /etc/apache2/sites-available/wordpress.conf
+#Enable the site
+a2ensite wordpress
+a2enmod rewrite
+a2dissite 000-default
+service apache2 reload
 #install diode and publish new site
 #curl -Ssf https://diode.io/install.sh | sh
 #diode publish -public 80:80 
