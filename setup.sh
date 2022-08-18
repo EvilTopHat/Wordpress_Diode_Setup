abf5430a5717463cbb65a1fdfa1da6d3cc2bc312
EvilTopHat <110784648+EvilTopHat@users.noreply.github.com>
2022-08-18 08:45:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -109,7 +109,7 @@ wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -
 chmod +x /usr/bin/wp
 
 #finish installation
-wp core install --allow-root --path="/srv/www/wordpress" --title="wordpress diode" --url="https://${diode_address}.diode.link" --admin_email="admin@localhost.com"  --admin_password="$wordpress_admin_password" --admin_user="admin"
+wp core install --allow-root --path="/srv/www/wordpress" --title="wordpress diode" --url="https://${diode_address}.diode.link" --admin_email="admin@localhost.com"  --admin_password="$wordpress_user_admin" --admin_user="admin"
 
 #install plugins
 wp plugin install wp-fail2ban --allow-root --path="/srv/www/wordpress"
